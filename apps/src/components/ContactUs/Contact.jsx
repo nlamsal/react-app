2dc65899f0ead55c15e4c4124cc24b5dcfea14d1
nlamsal <neerajan.lamsal@gmail.com>
2018-04-29 22:05:14 -0400

    latest changes

---
+++ apps/src/components/ContactUs/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Contact extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Contact Us.
+            </div>
+        );
+
+    }
+}
+
+export default Contact;
---
+++ apps/src/components/News/News.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class News extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Latest News Letters
+            </div>
+        );
+
+    }
+}
+
+export default News;
